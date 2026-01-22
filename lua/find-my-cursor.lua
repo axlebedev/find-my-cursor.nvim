c6daadd075e02e1a720b2d2bc3fb06d53f3b97ae
Alex Lebedev <alexey.broadcast@gmail.com>
2026-01-22 17:58:23 +0300

    Support nvim over old vim code, auto-translated by robot

---
+++ lua/find-my-cursor.lua
@@ -0,0 +1,142 @@
+local M = {}
+
+local defaultOpts = {
+  FindCursorPre = function() end,
+  FindCursorPost = function() end,
+  FindCursorDefaultColor = '#FF00FF',
+}
+M.config = defaultOpts
+
+local isActivated = false
+local timer_id = false
+local savedCursorlineBg = nil
+local savedCursorcolumnBg = nil
+
+M.setup = function(opts)
+  M.config = vim.tbl_deep_extend("force", defaultOpts, opts or {})
+end
+
+local function update_hl_bg(group_name, bg_val)
+  vim.api.nvim_set_hl(
+    0,
+    group_name,
+    vim.tbl_deep_extend(
+      "force",
+      vim.api.nvim_get_hl(0, { link = false, name = group_name }),
+      { bg = bg_val }
+    )
+  )
+end
+
+-- Return highlight attribute value for a group
+local function return_highlight_term(group, term)
+    return vim.api.nvim_get_hl(0, { name = group })[term]
+end
+
+-- Save window-local cursorline/cursorcolumn settings
+local function save_window_local_settings()
+  for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    vim.api.nvim_win_set_var(winid, 'savedSettings', {
+      cursorline = vim.wo[winid].cursorline,
+      cursorcolumn = vim.wo[winid].cursorcolumn,
+    })
+    vim.wo[winid].cursorline = false
+    vim.wo[winid].cursorcolumn = false
+  end
+end
+
+-- Save global settings and prepare
+local function save_settings()
+  M.config.FindCursorPre()
+  isActivated = true
+
+  save_window_local_settings()
+
+  if (not savedCursorlineBg) then
+    savedCursorlineBg = return_highlight_term('CursorLine', 'bg')
+  end
+  if (not savedCursorcolumnBg) then
+    savedCursorcolumnBg = return_highlight_term('CursorColumn', 'bg')
+  end
+end
+
+-- Restore window-local settings
+local function restore_window_local_settings()
+  for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local savedSettings = vim.api.nvim_win_get_var(winid, 'savedSettings')
+    if (savedSettings['cursorline'] ~= nil) then
+      vim.wo[winid].cursorline = savedSettings['cursorline']
+    end
+    if (savedSettings['cursorcolumn'] ~= nil) then
+      vim.wo[winid].cursorcolumn = savedSettings['cursorcolumn']
+    end
+  end
+end
+
+local function restore_settings(...)
+    if timer_id then vim.loop.timer_stop(timer_id) end
+    timer_id = false
+    if (isActivated == true) then
+      isActivated = false
+      if savedCursorlineBg then
+        update_hl_bg('CursorLine', savedCursorlineBg)
+        savedCursorlineBg = nil
+      end
+      if savedCursorcolumnBg then
+        update_hl_bg('CursorColumn', savedCursorcolumnBg)
+        savedCursorcolumnBg = nil
+      end
+      restore_window_local_settings()
+      M.config.FindCursorPost()
+    end
+
+    vim.api.nvim_clear_autocmds({ group = 'au_findcursor' })
+end
+
+M.FindCursor = function(...)
+    local args = {...}
+    local color = args[1] or M.config.FindCursorDefaultColor
+    local autoClearTimeoutMs = args[2] or 0
+    if not isActivated then
+      save_settings()
+      vim.opt_local.cursorline = true
+      vim.opt_local.cursorcolumn = true
+    end
+
+    if (color:sub(1, 1) == '#') then
+      update_hl_bg('CursorLine', color)
+      update_hl_bg('CursorColumn', color)
+    end
+
+    local au_findcursor = vim.api.nvim_create_augroup("au_findcursor", { clear = true })
+    vim.api.nvim_create_autocmd({
+      "BufNew",
+      "CursorMoved",
+      "CursorMovedI",
+      "BufLeave",
+      "CmdlineEnter",
+      "InsertEnter",
+      "InsertLeave",
+    }, {
+      pattern = "*",
+      group = au_findcursor,
+      callback = function()
+        restore_settings()
+      end,
+    })
+    if autoClearTimeoutMs > 0 then
+      timer_id = vim.loop.timer_start(autoClearTimeoutMs, function() restore_settings() end)
+    end
+end
+
+vim.api.nvim_create_user_command(
+  'FindCursor',
+  function(opts)
+    M.FindCursor(unpack(opts.fargs) or nil)
+  end,
+  {
+    nargs = '*', -- Zero or more argumentsbar = true,
+  }
+)
+
+return M
--- plugin/commands.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command('FindCursor', function(opts)
-  require('findcursor').find_cursor(opts.args ~= '' and opts.args or nil)
-end, {
-  nargs = '*',  -- Zero or more arguments
-  bar = true,   -- Allow | line continuation
-})
-
